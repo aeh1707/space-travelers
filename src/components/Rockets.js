0219e5a450af7b33320acad90677a3a00bdbb903
aeh1707d <abdelhadi.hireche@outlook.com>
2022-08-03 08:56:31 +0100

    Fetch rockets data and display it on the rockets view

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,10 +1,27 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import Nav from './Nav';
+import { fetchRockets } from '../redux/rockets/rockets';
 
-const Rockets = () => (
+const Rockets = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, []);
+  const rockets = useSelector((state) => state.rockets);
+  console.log(rockets);
+
+  return (
   <>
     <Nav />
     <h1>Rockets</h1>
+    {rockets.map((rocket) => (
+        <article key={rocket.id}>
+          <h2>{rocket.rocket_name}</h2>
+          <p>{ rocket.description }</p>
+        </article>
+      ))}
   </>
 );
-
+};
 export default Rockets;
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionReducer from './missions/missions';
+import rocketReducer from './rockets/rockets';
 
 const store = configureStore({
   reducer: {
     missions: missionReducer,
+    rockets: rocketReducer,
   },
 });
 
