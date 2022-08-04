e3fc3f6e9b0e059b30ddf8e00468d93d4c54a632
aeh1707d <abdelhadi.hireche@outlook.com>
2022-08-04 09:45:10 +0100

    Create rockets list

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,16 +1,33 @@
+import './MyProfile.css';
 import { useSelector } from 'react-redux';
 import Nav from './Nav';
 
 const MyProfile = () => {
   const missions = useSelector((state) => state.missions);
-  const joMissions = missions.filter((mission) => mission.reserved);
+  const rockets = useSelector((state) => state.rockets);
+  const joinedMissions = missions.filter((mission) => mission.reserved);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
   return (
     <>
       <Nav />
-      <h1>My Missions</h1>
-      <ul>
-        {joMissions.map((mission) => (<li key={mission.mission_id}>{mission.mission_name}</li>))}
-      </ul>
+      <div className="my-profile-container">
+        <div className="myprofile">
+          <section className="joined-missions">
+            <h2>My Missions</h2>
+            <ul>
+              {joinedMissions.map((mission) => (
+                <li key={mission.mission_id}>{mission.mission_name}</li>))}
+            </ul>
+          </section>
+          <section className="reserved-rockets">
+            <h2>My Rockets</h2>
+            <ul>
+              {reservedRockets.map((rocket) => (
+                <li key={rocket.id}>{rocket.rocket_name}</li>))}
+            </ul>
+          </section>
+        </div>
+      </div>
     </>
   );
 };
