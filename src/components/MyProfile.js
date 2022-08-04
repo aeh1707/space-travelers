45ccdc58072394caf8a98a03cf5f0cef974d1bb7
sajid-munawar <sajidmunawar666@gmail.com>
2022-08-04 15:00:53 +0500

    Remove extra nav

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,12 +1,10 @@
 import { useSelector } from 'react-redux';
-import Nav from './Nav';
 
 const MyProfile = () => {
   const missions = useSelector((state) => state.missions);
   const joMissions = missions.filter((mission) => mission.reserved);
   return (
     <>
-      <Nav />
       <h1>My Missions</h1>
       <ul>
         {joMissions.map((mission) => (<li key={mission.mission_id}>{mission.mission_name}</li>))}
