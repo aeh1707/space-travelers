5591a1dee38fcf728a6a0d900d5dbb92951b139f
aeh1707d <abdelhadi.hireche@outlook.com>
2022-08-04 07:34:06 +0100

    Add rocket rservation functionality

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,15 +1,19 @@
 import './Rockets.css';
-import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { PropTypes } from 'prop-types';
 import Nav from './Nav';
-import { fetchRockets } from '../redux/rockets/rockets';
+import { reserveRocket, leaveRocket } from '../redux/rockets/rockets';
 
-const Rockets = () => {
+const Rockets = (props) => {
+  const { rockets } = props;
+  console.log(rockets);
   const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchRockets());
-  }, []);
-  const rockets = useSelector((state) => state.rockets);
+  const handleReserveRocket = (id) => {
+    dispatch(reserveRocket(id));
+  };
+  const handleLeaveRocket = (id) => {
+    dispatch(leaveRocket(id));
+  };
 
   return (
     <>
@@ -20,7 +24,13 @@ const Rockets = () => {
             <img className="rocket-image" src={rocket.flickr_images} alt={`${rocket.rocket_name} rocket`} />
             <div className="rocket-info">
               <h2 className="rocket-name">{rocket.rocket_name}</h2>
-              <p className="rocket-description">{rocket.description}</p>
+              <p className="rocket-description">
+                {rocket.reserved && <span className="reserved-badge">Reserved</span> }
+                &nbsp;
+                {rocket.description}
+              </p>
+              {!rocket.reserved && <button type="button" className="rocket-reserve btn" onClick={() => handleReserveRocket(rocket.id)}>Reserver Rocket</button>}
+              {rocket.reserved && <button type="button" className="rocket-cancel btn" onClick={() => handleLeaveRocket(rocket.d)}>Cancel Reservation</button>}
             </div>
           </li>
         ))}
@@ -28,4 +38,9 @@ const Rockets = () => {
     </>
   );
 };
+
+Rockets.propTypes = {
+  rockets: PropTypes.instanceOf(Array).isRequired,
+};
+
 export default Rockets;
